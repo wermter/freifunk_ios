ce944e8ccc500da49ea03444db56fd00bae102a1
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2014-06-25 22:43:50 +0200

    Wiesbaden und Mainz

--- app/01_models/region.rb
+++ app/01_models/region.rb
@@ -1,16 +1,17 @@
 class Region < Struct.new(:key, :name, :zoom, :location, :data_url, :twitter, :homepage)
   ALL = [
-    Region.new(:hamburg,    "Hamburg",       7, [53.50, 10.00], "http://graph.hamburg.freifunk.net/nodes.json",          "FreifunkHH",       "http://hamburg.freifunk.net/"),
-    Region.new(:jena,       "Jena",          9, [50.90, 11.60], "http://map.freifunk-jena.de/ffmap/nodes.json",          "freifunkjena",     "http://freifunk-jena.de/"),
-    Region.new(:kiel,       "Kiel",          9, [54.30, 10.10], "http://freifunk.in-kiel.de/ffmap/nodes.json",           "freifunkkiel",     "http://freifunk.in-kiel.de/"),
-    Region.new(:luebeck,    "Lübeck",        8, [53.80, 10.70], "http://freifunk.metameute.de/map/nodes.json",           "freifunkluebeck",  "http://freifunk.metameute.de/"),
-    Region.new(:lueneburg,  "Lüneburg",      9, [53.24, 10.42], "http://freifunk-lueneburg.de/karte/nodes.json",         "freifunk",         "http://freifunk-lueneburg.de"),
-    Region.new(:paderborn,  "Paderborn",     9, [51.70,  8.75], "http://map.paderborn.freifunk.net/nodes.json",          "FreifunkPB",       "http://paderborn.freifunk.net/"),
-    Region.new(:wuppertal,  "Wuppertal",     9, [51.20,  7.15], "http://map.freifunk-wuppertal.net/nodes.json",          "ffwtal",           "http://freifunk-wuppertal.net/"),
-    Region.new(:dresden,    "Dresden",       9, [51.08, 13.73], "http://info.freifunk-dresden.de/info/freifunk_ios.json","ddmesh",           "http://www.ddmesh.de/"),
-    Region.new(:leipzig,    "Leipzig",      10, [51.33, 12.33], "http://ffmap.leipzig.freifunk.net/nodes.json",          "Freifunk_L",       "http://leipzig.freifunk.net/"),
-    Region.new(:battlemesh, "BattleMesh v7",10, [51.33, 12.33], "http://battlemesh.rund.freifunk.net/ffmap/nodes.json",  "freifunk",         "http://battlemesh.org/"),
-    Region.new(:magdeburg,  "Magdeburg",    11, [52.14, 11.65], "http://map.md.freifunk.net/nodes.json",                 "freifunkmd",       "http://md.freifunk.net"),
+    Region.new(:hamburg,    "Hamburg",         7, [53.50, 10.00], "http://graph.hamburg.freifunk.net/nodes.json",          "FreifunkHH",       "http://hamburg.freifunk.net/"),
+    Region.new(:jena,       "Jena",            9, [50.90, 11.60], "http://map.freifunk-jena.de/ffmap/nodes.json",          "freifunkjena",     "http://freifunk-jena.de/"),
+    Region.new(:kiel,       "Kiel",            9, [54.30, 10.10], "http://freifunk.in-kiel.de/ffmap/nodes.json",           "freifunkkiel",     "http://freifunk.in-kiel.de/"),
+    Region.new(:luebeck,    "Lübeck",          8, [53.80, 10.70], "http://freifunk.metameute.de/map/nodes.json",           "freifunkluebeck",  "http://freifunk.metameute.de/"),
+    Region.new(:lueneburg,  "Lüneburg",        9, [53.24, 10.42], "http://freifunk-lueneburg.de/karte/nodes.json",         "freifunk",         "http://freifunk-lueneburg.de"),
+    Region.new(:paderborn,  "Paderborn",       9, [51.70,  8.75], "http://map.paderborn.freifunk.net/nodes.json",          "FreifunkPB",       "http://paderborn.freifunk.net/"),
+    Region.new(:wuppertal,  "Wuppertal",       9, [51.20,  7.15], "http://map.freifunk-wuppertal.net/nodes.json",          "ffwtal",           "http://freifunk-wuppertal.net/"),
+    Region.new(:dresden,    "Dresden",         9, [51.08, 13.73], "http://info.freifunk-dresden.de/info/freifunk_ios.json","ddmesh",           "http://www.ddmesh.de/"),
+    Region.new(:leipzig,    "Leipzig",        10, [51.33, 12.33], "http://ffmap.leipzig.freifunk.net/nodes.json",          "Freifunk_L",       "http://leipzig.freifunk.net/"),
+    Region.new(:battlemesh, "BattleMesh v7",  10, [51.33, 12.33], "http://battlemesh.rund.freifunk.net/ffmap/nodes.json",  "freifunk",         "http://battlemesh.org/"),
+    Region.new(:magdeburg,  "Magdeburg",      11, [52.14, 11.65], "http://map.md.freifunk.net/nodes.json",                 "freifunkmd",       "http://md.freifunk.net"),
+    Region.new(:mzwi,       "Mainz/Wiesbaden",11, [50.03,  8.24], "http://map.freifunk-mainz.de/nodes.json",               "freifunkmainz",    "http://www.freifunk-mainz.de"),
   ]
 
   def self.all
